06e637813551ac7e106d937bba924b5b585cd224
bismiswalih <bismiswalih@gmail.com>
2022-04-13 03:22:43 -0700

    commit

--- js/snackbar.js
+++ js/snackbar.js
@@ -2,5 +2,4 @@ function snackBar() {
     var x = document.getElementById("snackbar");
     x.className = "show";
     setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
-    alert("hgugj");
 }
